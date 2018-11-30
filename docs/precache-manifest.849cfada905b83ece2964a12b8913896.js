9bb6fba8df344ea90c9563e2d0c68eb5430b34b4
Zsolt Kerekes <zsolt_kerekes@epam.com>
2018-11-30 10:56:08 +0100

    updated react version

---
+++ docs/precache-manifest.849cfada905b83ece2964a12b8913896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4d11f2df400cfd02937",
+    "url": "/music-finder/static/js/runtime~main.f4d11f2d.js"
+  },
+  {
+    "revision": "c75b9b4fbd1e6afc7dbf",
+    "url": "/music-finder/static/js/main.c75b9b4f.chunk.js"
+  },
+  {
+    "revision": "09a9c3bbe33a88271d9d",
+    "url": "/music-finder/static/js/1.09a9c3bb.chunk.js"
+  },
+  {
+    "revision": "c75b9b4fbd1e6afc7dbf",
+    "url": "/music-finder/static/css/main.ab2b1e08.chunk.css"
+  },
+  {
+    "revision": "09a9c3bbe33a88271d9d",
+    "url": "/music-finder/static/css/1.7b61aca6.chunk.css"
+  },
+  {
+    "revision": "77316f4506bc59803580e8df84892680",
+    "url": "/music-finder/index.html"
+  }
+];
